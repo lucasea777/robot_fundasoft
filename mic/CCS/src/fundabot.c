7a2d90a9ac1cf036593996224557f89518f30931
Luks777 <lucas.e.a777@gmail.com>
2016-03-07 20:32:18 -0300

    preprocesor

--- mic/CCS/src/fundabot.c
+++ mic/CCS/src/fundabot.c
@@ -1,6 +1,8 @@
 // fuente: http://www.forosdeelectronica.com/f24/envio-hola-ccs-usb-atraves-hid-18f2550-46955/
 // no es necesario modificar otro archivo aparte de este!!!!!
 
+//comentar si se esta usando la pcb en vez de la proto!
+#define PROTOBOARD
 #include <18f2550.h>           //archivo de cabecera
 
 //#fuses HSPLL,MCLR,NOWDT,NOPROTECT,NOLVP,NODEBUG,USBDIV,PLL5,CPUDIV1,NOVREGEN,NOPBADEN // fuses  configurados
@@ -9,12 +11,16 @@
 #use delay(clock=48000000)     // el clock que tendremos a la entrada del CPU compatible con USB 2.0
 //#use rs232(baud=9600, xmit=PIN_C6, rcv=PIN_C7, BITS=8) //para debuguear las salidas del printf,puts etc.
 
+#ifdef PROTOBOARD
 #include <\lib\flex_lcd_2.c>
+#endif PROTOBOARD
 
 #define USB_CONFIG_HID_RX_SIZE 10  //lucas!!!
 #define USB_CONFIG_HID_TX_SIZE 10  //lucas!!!
 
 #DEFINE USB_HID_DEVICE  TRUE // Si usar HID 
+#define USB_CONFIG_PID  0x0020
+#define  USB_CONFIG_VID 0x0461
 
 //Habilitar la Transferencia
 #define USB_EP1_TX_ENABLE  USB_ENABLE_INTERRUPT   //Activa el punto final 1 para transferencias por bloque 
@@ -88,21 +94,25 @@ void checkear_terminal() {
    //#include <terminal_hid.c>
    if (usb_enumerated()) {
       if(!avise) {
-         lcd_gotoxy(1,1);printf(lcd_putc ,"Enumere!! :D");
+      	 #ifdef PROTOBOARD
+         lcd_gotoxy(1,1);printf(lcd_putc ,"Enumere!! :D"); //<<protoboard>>
+         #endif PROTOBOARD
          avise = true;
       }
       //usb_puts(1,Mensaje,4,100);
       if (usb_kbhit(1)) {
+      	usb_gets(1, &recivido, PACKAGE_LENGTH, 100);
+         #ifdef PROTOBOARD
          lcd_gotoxy(1,1);printf(lcd_putc ,"kbhit!! :D");
-         usb_gets(1, &recivido, PACKAGE_LENGTH, 100);
          lcd_gotoxy(1,1);printf(lcd_putc ,"                 ");
          lcd_gotoxy(1,1);printf(lcd_putc , "Recivi %s", recivido);
+         #endif
    
       }
    }
-   if (avise && !usb_enumerated()) {
+   /*if (avise && !usb_enumerated()) {   	
       lcd_gotoxy(1,1);printf(lcd_putc ,"Desenumere!! :D");
-   }
+   }*/
 }
 
 #task(rate=1000ms, max=10ms)
@@ -125,18 +135,20 @@ void mandar_boton() {
 }
 
 void main() {
-
-   lcd_init();
-
    setup_adc_ports(NO_ANALOGS);  // desactivando puertos analogicos   
    //set_tris_b(0x0);              // configurando los puertos como salidas
    //output_b (0);                 // saca un nivel bajo de salida en los puertos
 
+   #ifdef PROTOBOARD
+   lcd_init();
+   lcd_gotoxy(1,1);printf(lcd_putc ,"Espero... ");
+   #endif  
 
-   lcd_gotoxy(1,1);printf(lcd_putc ,"Espero... 1");
    usb_init(); //inicializa y espera a ser encendido
    //usb_task (); //lucas!
-   lcd_gotoxy(1,1);printf(lcd_putc ,"Inicializado");
+   #ifdef PROTOBOARD
+   lcd_gotoxy(1,1);printf(lcd_putc ,"Inicializado"); //<<protoboard>>
+   #endif
    
    //usb_wait_for_enumeration(); //ahora espera hasta ser enumerado (reconocido por la PC)    
    //lcd_gotoxy(1,1);printf(lcd_putc ,"Enumere!! :D");
